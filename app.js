6ba87ac64dc6ac23648bc26620aac3094bab4220
Jeya Subalakshmi <jslakshmi.mca@gmail.com>
2024-04-22 16:39:01 +0200

    Pipeline created

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Hello again AWS'));
+app.get('/', (req, res) => res.send('Hello World! Hello again AWS.....pipeline created'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
